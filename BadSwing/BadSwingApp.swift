9a0b9e1ab163f0e55307d412b5ea56c64c0b1ded
fahmi <fahmianton@gmail.com>
2026-04-06 17:53:26 +0700

    Initial Commit

---
+++ BadSwing/BadSwingApp.swift
@@ -0,0 +1,17 @@
+//
+//  BadSwingApp.swift
+//  BadSwing
+//
+//  Created by Muhammad Fahmi on 06/04/26.
+//
+
+import SwiftUI
+
+@main
+struct BadSwingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BadSwing/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BadSwing
+//
+//  Created by Muhammad Fahmi on 06/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
